bb245ad2a719704ab20d6314ffcfdcb6d5492fc7
FilipeIzidorio <fmotor312@gmail.com>
2025-09-23 14:39:48 -0300

    first commit

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-DATABASE_URL = "postgresql://postgres:admin@localhost:5432/ms-cardapio"
+DATABASE_URL = "postgresql://dbcardapio_w1xz_user:k6ZzjAQGBYUPwmubQRBFiRcCZgekjvSM@dpg-d39dl0ogjchc73dm9320-a.oregon-postgres.render.com/dbcardapio_w1xz"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
